ac4d4bfc2a11fb305236399ea6ad52f3a34a2e8e
DimaViatkin <Viatkindima@gmail.com>
2025-07-27 10:05:29 +0300

    #45

--- cinewave/settings.py
+++ cinewave/settings.py
@@ -31,7 +31,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+allowed_hosts_string = os.getenv('ALLOWED_HOSTS', '')
+# Разделяем строку по запятым, обрезаем пробелы и фильтруем пустые значения
+ALLOWED_HOSTS = [host.strip() for host in allowed_hosts_string.split(',') if host.strip()]
 
 
 # Application definition
